d2c43d478f8f733ea0457d391a5d0a46ce3b5b7a
TeodorMoiseev <femoiseev@gmail.com>
2019-02-24 17:24:38 +0300

    [TeodorMoiseev] Improve transformer quality on he-en task.

--- scripts/evaluation/evaluate_transformer_vocalization.sh
+++ scripts/evaluation/evaluate_transformer_vocalization.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
-{
-    fairseq-generate ../../data/processed/vocalization \
-        --user-dir ../../models \
-        --path ../../checkpoints/transformer_vocalization/checkpoint_best.pt \
-        --batch-size 512 --beam 5 --remove-bpe --raw-text | tee /tmp/gen.out
-} &> /dev/null
-
-grep ^H /tmp/gen.out | cut -f3- > /tmp/gen.out.sys
-grep ^T /tmp/gen.out | cut -f2- > /tmp/gen.out.ref
-fairseq-score --sys /tmp/gen.out.sys --ref /tmp/gen.out.ref | tee ../../results/transformer_vocalization.txt
+fairseq-generate ../../data/processed/vocalization \
+    --user-dir ../../models \
+    --path ../../checkpoints/transformer_vocalization/checkpoint_best.pt \
+    --batch-size 512 --beam 5 --remove-bpe --raw-text \
+    --quiet --log-format=tqdm | tee ../../results/transformer_vocalization.txt
--- scripts/training/train_transformer_he-en.sh
+++ scripts/training/train_transformer_he-en.sh
@@ -2,9 +2,9 @@
 CUDA_VISIBLE_DEVICES=0 fairseq-train ../../data/processed/he-en \
   --user-dir ../../models --task translation --raw-text \
   -a transformer_he-en --optimizer adam --lr 0.0005 -s he -t en \
-  --label-smoothing 0.1 --dropout 0.3 --max-tokens 4000 \
+  --label-smoothing 0.1 --max-tokens 4000 \
   --min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0001 \
-  --criterion label_smoothed_cross_entropy --max-update 50000 \
+  --criterion label_smoothed_cross_entropy --me 200 \
   --warmup-updates 4000 --warmup-init-lr '1e-07' \
   --adam-betas '(0.9, 0.98)' --save-dir ../../checkpoints/transformer_he-en \
   --keep-last-epochs 100
