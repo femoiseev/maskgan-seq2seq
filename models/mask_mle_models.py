16e42eb28bf0f95561965cb5eda0cf30d0d03572
canorbal <canorbal@gmail.com>
2019-03-05 23:23:16 +0000

    work in progress

--- models/mask_mle_models.py
+++ models/mask_mle_models.py
@@ -21,22 +21,21 @@ class MLETransformerEncoder(FairseqEncoder):
 
     def __init__(self, args, src_dictionary, dst_dictionary, 
                  src_embed_tokens, dst_embed_tokens, left_pad=True):
+        
+        print("created MLE Encoder")
         super().__init__(None)
-        
-        self.SPLIT_SYMBOL = "<SPLIT>"
-        self.src_dictionary = deepcopy(src_dictionary)
-        self.dst_dictionary = deepcopy(dst_dictionary)
-        
-        self.dst_dictionary.add_symbol("<MASK>", n=1)
-        
-        self.encoder = TransformerEncoder(args, self.src_dictionary,
+            
+        self.src_dictionary = src_dictionary
+        self.dst_dictionary = dst_dictionary
+        self.encoder = TransformerEncoder(args, src_dictionary,
                                            src_embed_tokens, left_pad=left_pad)
         
-        self.masked_encoder = TransformerEncoder(args, self.dst_dictionary,
+        self.masked_encoder = TransformerEncoder(args, dst_dictionary,
                                                  dst_embed_tokens, left_pad=left_pad)
         
+        
 
-    def forward(self, src_tokens, src_lengths):
+    def forward(self, src_tokens, src_lengths, masked_tgt, tgt_lengths):
         """
         Args:
             src_tokens (LongTensor): tokens in the source language of shape
@@ -52,24 +51,45 @@ class MLETransformerEncoder(FairseqEncoder):
         """
         # embed tokens and positions
            
-        src_mask = src_tokens == self.src_dictionary.index(sym=self.SPLIT_SYMBOL)
-        print(src_mask[0])
-        print(src_mask[1])
-        print(src_mask[2])
-        print(src_mask[3])
+#         src_mask = src_tokens == self.src_dictionary.index(sym=self.SPLIT_SYMBOL)
+#         print(self.src_dictionary.string(src_tokens[:, 1]))
+#         print(src_mask[0])
+#         print(src_mask[1])
+#         print(src_mask[2])
+#         print(src_mask[3])
+#         print("*" * 30)
+#         for i in range(128):
+#             print(self.src_dictionary.index(src_tokes[0][i], endline=" "))
+
+        print("*" * 40)
+        print("src")
+        print(self.src_dictionary.string(src_tokens))
+        print("tgt")
+        print(self.dst_dictionary.string(masked_tgt))
+        print("*" * 40)
         
         src_dict = self.encoder(src_tokens, src_lengths)
+#         print("usual encoder ok")
+
+        print("*" * 40)
+        print(torch.max(src_lengths))
+        print(tgt_lengths.size())
+        print(torch.max(tgt_lengths))
+        print("*" * 40)
+        dst_dict = self.masked_encoder(masked_tgt, tgt_lengths)
+        print("masked ok")
+        print("masked tgt")
+        print(masked_tgt.size())
+        print("src ")
+        print(src_tokens.size())
+        print(torch.cat([src_dict['encoder_out'], dst_dict['encoder_out']], dim=-1).size())
         
-        mask_prob = np.random.uniform()
-        masks = np.random.binomial(n=1, p=mask_prob, size=dst_tokens.shape)
-        masks = torch.LongTensor(masks)
-        dst_tokens = masks * dst_tokens
-        dst_dict = self.masked_encoder(dst_tokens, dst_lengths)
         
         res = {
-            'encoder_out': torch.stack([src_dict['encoder_out'], dst_dict['encoder_out']], dim=-1),
+            'encoder_out': torch.cat([src_dict['encoder_out'], dst_dict['encoder_out']], dim=-1),
             'encoder_padding_mask': None
         }
+        return res
         
 
     def reorder_encoder_out(self, encoder_out, new_order):
--- scripts/training/train_mle_transformer_vocalization.sh
+++ scripts/training/train_mle_transformer_vocalization.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 CUDA_VISIBLE_DEVICES=0 fairseq-train ../../data/processed/vocalization \
-  --user-dir ../../models --task translation --raw-text \
+  --user-dir ../../models --task mask_mle_translation --raw-text \
   -a mle_transformer_vocalization --optimizer adam --lr 0.0005 -s he -t voc \
   --label-smoothing 0.1 --dropout 0.3 --max-tokens 4000 \
   --min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0001 \
