d7bfdc2bc34067e6c31e1c861b43819079968dca
canorbal <canorbal@gmail.com>
2019-03-05 23:22:20 +0000

    task for mask mle

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,11 @@
+import os
 import numpy
 import torch
+import itertools
+from fairseq.data import (IndexedCachedDataset,
+    IndexedDataset,
+    IndexedRawTextDataset,
+    LanguagePairDataset,)
 from fairseq.models import register_model_architecture
 from fairseq.models import FairseqModel, register_model, FairseqEncoder
 from fairseq.models.transformer import (TransformerEncoder,
@@ -14,6 +20,7 @@ from .mask_mle_models import MLETransformerEncoder
 class MLETransformer(FairseqModel):
 
     def __init__(self, encoder, decoder):
+        print("created MLE Transformer")
         super().__init__(encoder, decoder)
 
     @staticmethod
@@ -82,9 +89,7 @@ class MLETransformer(FairseqModel):
 
         src_dict, tgt_dict = task.source_dictionary, task.target_dictionary
 
-        def build_embedding(dictionary, embed_dim, path=None, add_mask=False):
-            if add_mask:
-                dictionary.add_symbol("<<MASK>>", n=1)
+        def build_embedding(dictionary, embed_dim, path=None):
                 
             num_embeddings = len(dictionary)
             padding_idx = dictionary.pad()
@@ -92,8 +97,6 @@ class MLETransformer(FairseqModel):
             # if provided, load from preloaded dictionaries
             if path:
                 embed_dict = utils.parse_embedding(path)
-                if add_mask:
-                    dictionary.add_symbol("<<MASK>>", n=1)
                 utils.load_embedding(embed_dict, dictionary, emb)
             return emb
 
@@ -102,7 +105,7 @@ class MLETransformer(FairseqModel):
          )
             
         tgt_embed_tokens = build_embedding(
-            tgt_dict, args.decoder_embed_dim, args.decoder_embed_path, add_mask=True
+            tgt_dict, args.encoder_embed_dim, args.decoder_embed_path
         )
         
         decoder_embed_tokens = build_embedding(
@@ -116,7 +119,13 @@ class MLETransformer(FairseqModel):
                                         src_embed_tokens, tgt_embed_tokens)
         
         decoder = TransformerDecoder(args, tgt_dict, decoder_embed_tokens)
-        return TransformerModel(encoder, decoder)
+        
+        return MLETransformer(encoder, decoder)
+    
+    def forward(self, src_tokens, src_lengths, masked_tgt, tgt_lengths, prev_output_tokens):
+        encoder_out = self.encoder(src_tokens, src_lengths, masked_tgt, tgt_lengths)
+        decoder_out = self.decoder(prev_output_tokens, encoder_out)
+        return decoder_out
 
         
 @register_model_architecture('mle_transformer', 'mle_transformer_vocalization')
@@ -205,3 +214,123 @@ def tr_he_en_architecture(args):
 
     args.decoder_output_dim = getattr(args, 'decoder_output_dim', args.decoder_embed_dim)
     args.decoder_input_dim = getattr(args, 'decoder_input_dim', args.decoder_embed_dim)
+    
+    
+from fairseq import utils, options
+from fairseq.tasks.translation import TranslationTask
+from fairseq.tasks import register_task
+from dataloaders.mle_dataset import MLELanguagePairDataset
+
+@register_task("mask_mle_translation")
+class MaskMLETranslationTask(TranslationTask):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    
+    @classmethod
+    def setup_task(cls, args, **kwargs):
+        """Setup the task (e.g., load dictionaries).
+        Args:
+            args (argparse.Namespace): parsed command-line arguments
+        """
+#         args.left_pad_source = options.eval_bool(args.left_pad_source)
+#         args.left_pad_target = options.eval_bool(args.left_pad_target)
+        args.left_pad_source = True
+        args.left_pad_target = True
+
+        # find language pair automatically
+        if args.source_lang is None or args.target_lang is None:
+            args.source_lang, args.target_lang = data_utils.infer_language_pair(args.data[0])
+        if args.source_lang is None or args.target_lang is None:
+            raise Exception('Could not infer language pair, please provide it explicitly')
+
+        # load dictionaries
+        src_dict = cls.load_dictionary(os.path.join(args.data[0], 'dict.{}.txt'.format(args.source_lang)))
+        tgt_dict = cls.load_dictionary(os.path.join(args.data[0], 'dict.{}.txt'.format(args.target_lang)))
+        cls.mask_idx = tgt_dict.add_symbol('<MASK>',  1)
+        
+        assert src_dict.pad() == tgt_dict.pad()
+        assert src_dict.eos() == tgt_dict.eos()
+        assert src_dict.unk() == tgt_dict.unk()
+        print('| [{}] dictionary: {} types'.format(args.source_lang, len(src_dict)))
+        print('| [{}] dictionary: {} types'.format(args.target_lang, len(tgt_dict)))
+
+        return cls(args, src_dict, tgt_dict)
+ 
+    
+    def load_dataset(self, split, combine=False, **kwargs):
+        """Load a given dataset split.
+        Args:
+            split (str): name of the split (e.g., train, valid, test)
+        """
+
+        def split_exists(split, src, tgt, lang, data_path):
+            filename = os.path.join(data_path, '{}.{}-{}.{}'.format(split, src, tgt, lang))
+            if self.args.raw_text and IndexedRawTextDataset.exists(filename):
+                return True
+            elif not self.args.raw_text and IndexedDataset.exists(filename):
+                return True
+            return False
+
+        def indexed_dataset(path, dictionary):
+            if self.args.raw_text:
+                return IndexedRawTextDataset(path, dictionary)
+            elif IndexedDataset.exists(path):
+                if self.args.lazy_load:
+                    return IndexedDataset(path, fix_lua_indexing=True)
+                else:
+                    return IndexedCachedDataset(path, fix_lua_indexing=True)
+            return None
+
+        src_datasets = []
+        tgt_datasets = []
+
+        data_paths = self.args.data
+
+        for dk, data_path in enumerate(data_paths):
+            for k in itertools.count():
+                split_k = split + (str(k) if k > 0 else '')
+
+                # infer langcode
+                src, tgt = self.args.source_lang, self.args.target_lang
+                if split_exists(split_k, src, tgt, src, data_path):
+                    prefix = os.path.join(data_path, '{}.{}-{}.'.format(split_k, src, tgt))
+                elif split_exists(split_k, tgt, src, src, data_path):
+                    prefix = os.path.join(data_path, '{}.{}-{}.'.format(split_k, tgt, src))
+                else:
+                    if k > 0 or dk > 0:
+                        break
+                    else:
+                        raise FileNotFoundError('Dataset not found: {} ({})'.format(split, data_path))
+
+                src_datasets.append(indexed_dataset(prefix + src, self.src_dict))
+                tgt_datasets.append(indexed_dataset(prefix + tgt, self.tgt_dict))
+
+                print('| {} {} {} examples'.format(data_path, split_k, len(src_datasets[-1])))
+
+                if not combine:
+                    break
+
+        assert len(src_datasets) == len(tgt_datasets)
+
+        if len(src_datasets) == 1:
+            src_dataset, tgt_dataset = src_datasets[0], tgt_datasets[0]
+        else:
+            sample_ratios = [1] * len(src_datasets)
+            sample_ratios[0] = self.args.upsample_primary
+            src_dataset = ConcatDataset(src_datasets, sample_ratios)
+            tgt_dataset = ConcatDataset(tgt_datasets, sample_ratios)
+
+        self.datasets[split] = MLELanguagePairDataset(
+            src_dataset, src_dataset.sizes, self.src_dict,
+            tgt_dataset, tgt_dataset.sizes, self.tgt_dict,
+            left_pad_source=self.args.left_pad_source,
+            left_pad_target=self.args.left_pad_target,
+            max_source_positions=self.args.max_source_positions,
+            max_target_positions=self.args.max_target_positions,
+        )
+
+    
+    def build_dataset_for_inference(self, src_tokens, src_lengths):
+        return MLELanguagePairDataset(src_tokens, src_lengths, self.source_dictionary)
